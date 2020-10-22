7bf8c133158e5d676d0e7decf4b6f6d8adf2109a
Artem Inzhyyants <36314070+artem1205@users.noreply.github.com>
2020-10-22 23:31:32 +0300

    new project for N-Development
    Models, fields and relation

---
+++ addons_project/openacademy/models/openacademy.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+
+from datetime import datetime
+from odoo import fields, models
+
+
+class OpenacademyCourse(models.Model):
+    _name = "openacademy.course"
+
+    name = fields.Char(reqired=True)
+    description = fields.Text
+    responsible_id = fields.Many2one('openacademy.partner')
+    session_ids = fields.One2many('openacademy.session')
+    level = fields.Selection([('easy', 'Easy'),
+                              ('medium', 'Medium'),
+                              ('hard', 'Hard')])
+
+
+class OpenacademySession(models.Model):
+    _name = "openacademy.session"
+
+    name = fields.Char(reqired=True)
+    state = fields.Selection([('draft', 'Draft'),
+                              ('confirmed', 'Confirmed'),
+                              ('done', 'Done')],
+                             default='draft')
+    start_date = fields.Date(default = datetime.today())
+    end_date = fields.Date(default=datetime.today())
+    duration = fields.Float(default = 1)
+    instruction_id = fields.Many2one('openacademy.partner')
+    course_id = fields.Many2one('openacademy.course',
+                                required=True,
+                                ondelete="cascade")
+    attendee_ids = fields.Many2many('openacademy.partner')
+
+
+class OpenacademyPartner(models.Model):
+    _name = "openacademy.partner"
+
+    name = fields.Char('Name')
+    instructor = fields.Boolean('Instructor')
+    session_id = fields.Many2many('openacademy.session',
+                                  readonly=True)
