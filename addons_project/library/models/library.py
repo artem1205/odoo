db43e651ab1278ab57a76540eb0d4f617a6fbbab
Artem Inzhyyants <36314070+artem1205@users.noreply.github.com>
2020-10-23 21:44:24 +0300

    update library.py
    added new related fields

--- addons_project/library/models/library.py
+++ addons_project/library/models/library.py
@@ -1,37 +1,43 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class LibraryBook(models.Model):
     _name = "library.book"
 
     name = fields.Char('Name')
-    author_ids = fields.Many2many('library.partner')
-    edition_date = fields.Date('Edition Time')
+    author_ids = fields.Many2many('library.partner', string='Author')
+    edition_date = fields.Date('Edition Date')
     isbn = fields.Char('ISBN')
-    publisher_id = fields.Many2one('library.publisher')
-    rental_ids = fields.One2many('library.rental')
+    publisher_id = fields.Many2one('library.publisher', string='Publisher')
+    rental_ids = fields.One2many('library.rental', 'book_id', string='rental #')
 
 
 class LibraryRental(models.Model):
     _name = "library.rental"
 
-    customer_id = fields.Many2one('library.partner', store=True, required=True)
-    book_id = fields.Many2one('library.book', store=True)
-    rental_date = fields.Date('Rental Date', store=True, required=True)
-    return_date = fields.Date('Return Date', store=True, required=True)
+    customer_id = fields.Many2one('library.partner', string='Customer',
+                                  store=True, required=True)
+    book_id = fields.Many2one('library.book', 'rental_ids', store=True)
+    rental_date = fields.Date(string='Rental Date', store=True, required=True)
+    return_date = fields.Date(string='Return Date', store=True, required=True)
+    customer_address = fields.Text('Customer Address', related='customer_id.address')
+    customer_email = fields.Char('Customer Email', related='customer_id.email')
+    book_authors = fields.Char('Book authors', related='book_id.name')
+    book_edition_date = fields.Date('Edition Date', related='book_id.edition_date')
+    book_publisher = fields.Char('Publisher', related='book_id.name')
 
 
 class LibraryPartner(models.Model):
-    _name = "library.rental"
+    _name = "library.partner"
 
-    name = fields.Char('Name', required=True)
-    email = fields.Char('E-mail')
-    address = fields.Text('Address')
+    name = fields.Char('Name', required=True, store=True)
+    email = fields.Char('E-mail', store=True)
+    address = fields.Text('Address', store=True)
     partner_type = fields.Selection([('customer', 'Customer'),
                                      ('author', 'Author')])
-    rental_ids = fields.One2many('library.rental')
+    rental_ids = fields.One2many('library.rental', 'customer_id', string='rental #')
 
 
 class LibraryPublisher(models.Model):
