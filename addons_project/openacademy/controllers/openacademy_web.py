c460448daff522f1f7b5fe26751e63789d428a06
Artem Inzhyyants <36314070+artem1205@users.noreply.github.com>
2020-11-15 02:03:20 +0200

    Openacademy web views added

--- addons_project/openacademy/controllers/openacademy_web.py
+++ addons_project/openacademy/controllers/openacademy_web.py
@@ -14,18 +14,32 @@ from odoo.addons.website.controllers.main import QueryURL
 from odoo.http import request
 from odoo.tools import html2plaintext
 
-#
-# class OpenacademyWebform(http.Controller):
-#     @http.route('/openacademy/courses', auth='public', website=True)
-#     def courses(self, **kw):
-#         Courses = http.request.env['openacademy.course']
-#         return http.request.render('library.index', {
-#             'books': Books.search([('book_state', '!=', 'lost')])
-#         })
-    # def books(self, **kw):
-    #     books = request.env['library.copy'].sudo().search([])
-    #     html_result = '<html><body><ul>'
-    #     for book in books:
-    #         html_result += "<li> %s </li>" % book.name
-    #     html_result += '</ul></body></html>'
-    #     return html_result
+
+class OpenacademyWebform(http.Controller):
+    @http.route('/openacademy/courses', auth='public', website=True)
+    def courses(self, **kw):
+        Courses = http.request.env['openacademy.course']
+        return http.request.render('openacademy.courses', {
+            'courses': Courses.search([])
+        })
+
+    @http.route('/openacademy/courses/<int:id>', auth='public', website=True)
+    def course(self, id):
+        course = http.request.env['openacademy.course'].search([('id', '=', id)])
+        return http.request.render('openacademy.course_page', {
+            'course': course
+        })
+
+    @http.route('/openacademy/sessions', auth='public', website=True)
+    def courses(self, **kw):
+        Sessions = http.request.env['openacademy.session']
+        return http.request.render('openacademy.sessions', {
+            'sessions': Sessions.search([])
+        })
+
+    @http.route('/openacademy/sessions/<int:id>', auth='public', website=True)
+    def course(self, id):
+        session = http.request.env['openacademy.session'].search([('id', '=', id)])
+        return http.request.render('openacademy.session_page', {
+            'session': session
+        })
