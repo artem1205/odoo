92534afdb8ca6e38325b92dba55de65a62014351
Artem Inzhyyants <36314070+artem1205@users.noreply.github.com>
2020-11-28 00:33:55 +0200

    ADD improvement for .xls report
    Change view xls to add borders

---
+++ addons_project/change_xls_view/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "bordered_xls_report",
+
+    'summary': """
+        bordered_xls_report""",
+
+    'description': """
+    Change view xls to add borders for all cells and remove grey background for headers
+    """,
+
+    'author': "Artem Inzhyyants",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'web'],
+}
